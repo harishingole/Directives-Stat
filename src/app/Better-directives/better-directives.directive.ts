1c5fd94859a450609f64de27ec0a3a9893b0c2eb
Harish Ingole <harish.ingole@vinsys.com>
2021-04-02 18:43:27 +0530

    "Custom Property Binding"

--- src/app/Better-directives/better-directives.directive.ts
+++ src/app/Better-directives/better-directives.directive.ts
@@ -1,21 +1,27 @@
-import { Directive, Renderer2,OnInit,ElementRef, HostListener, HostBinding } from '@angular/core';
+import { Directive, Renderer2,OnInit,ElementRef, HostListener, HostBinding, Input } from '@angular/core';
 
 @Directive({
   selector: '[appBetterDirectives]'
 })
 export class BetterDirectivesDirective implements OnInit{
 
+  
+  @Input() defaultColor: string = "transparent";
+  @Input('appBetterDirectives') highlightColor: string = "blue";
+  @HostBinding('style.backgroundColor') backgroundColor:string;
+  
   constructor(private renderer: Renderer2, private eleRef: ElementRef) { }
-  @HostBinding('style.backgroundColor') backgroundColor:string = 'transparent';
+
   ngOnInit(){
+    this.backgroundColor =  this.defaultColor;
     // this.renderer.setStyle(this.eleRef.nativeElement, 'backgroundColor', 'blue');
   }
   @HostListener('mouseenter') mousehover(event: MouseEvent){
     // this.renderer.setStyle(this.eleRef.nativeElement, 'backgroundColor', 'blue');
-    this.backgroundColor = 'blue';
+    this.backgroundColor = this.highlightColor;
   }
   @HostListener('mouseout') mouseleave(event: MouseEvent){
     // this.renderer.setStyle(this.eleRef.nativeElement, 'backgroundColor', 'transparent');
-    this.backgroundColor = 'transparent';
+    this.backgroundColor = this.defaultColor;
   }
 }
